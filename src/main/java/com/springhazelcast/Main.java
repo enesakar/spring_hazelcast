25af0909dbd756fd3f48bb0bd1648d04f3f3d461
Enes Akar <enesakar@gmail.com>
2012-04-05 10:21:19 +0300

    first commit

---
+++ src/main/java/com/springhazelcast/Main.java
@@ -0,0 +1,18 @@
+package com.springhazelcast;
+
+import com.hazelcast.core.IMap;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new GenericXmlApplicationContext("beans.xml");
+        TestBean testBean = (TestBean) ctx.getBean("springTestBean");
+        System.out.println(testBean.getResult());
+
+        IMap map = (IMap) ctx.getBean("usermap");
+        map.put("somekey","somevalue");
+        System.out.println(map.get("somekey"));
+    }
+}
---
+++ src/main/java/com/springhazelcast/TestBean.java
@@ -0,0 +1,13 @@
+package com.springhazelcast;
+
+public class TestBean {
+    private String result;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}
